6b9f7aca67c46182517d60202e19f14931997da4
Senelie-Anisha <senelieanisha@gmail.com>
2025-08-26 12:35:04 +0530

    feature changes in news app

--- app/src/main/java/com/example/lktodayfinal/settings.kt
+++ app/src/main/java/com/example/lktodayfinal/settings.kt
@@ -8,20 +8,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class menu : AppCompatActivity() {
+class settings : AppCompatActivity() {
 
     private lateinit var back: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.menu)) { v, insets ->
+        setContentView(R.layout.activity_settings)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.settings)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        // Button click → go to LoginPage
         back = findViewById(R.id.back)
         back.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
